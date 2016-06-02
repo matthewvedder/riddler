dcd10dc2e7cb53ae98d032fd1dbf14bba6137be4
veddster <matthewvedder@gmail.com>
2016-06-01 20:42:51 -0400

    Add nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ riddle_data = [
   { question: 'What goes up as it comes down?', answer: 'Snow', upvotes: 25 },
   { question: 'Poor people have it. Rich people need it. If you eat it you die. What is it?', answer: 'Nothing', upvotes: 30 },
   { question: 'I’m tall when I’m young and I’m short when I’m old. What am I?', answer: 'A Candle', upvotes: 15 },
-  { question: 'Mary’s father has 5 daughters – Nana, Nene, Nini, Nono. What is the fifth daughters name?', answer: 'Mary', upvotes: 10 },
+  { question: "Mary’s father has 5 daughters – Nana, Nene, Nini, Nono. What is the fifth daughter's name?", answer: 'Mary', upvotes: 10 },
   { question: 'How can a pants pocket be empty and still have something in it?', answer: 'It can have a hole in it!', upvotes: 3 },
   { question: 'What goes up when rain comes down?', answer: 'An umbrella', upvotes: 10 },
   { question: 'What is the longest word in the dictionary?', answer: "Smiles, because there is a mile between each ‘s’", upvotes: 0 },
---
+++ db/triangle.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+
+def triangle_type()
+  "Har!"
+end
+
+class TriangleTest < Test::Unit::TestCase
+
+  def test_length
+    assert_equaltriangle_type(), 'Har!')
+  end
+
+end
